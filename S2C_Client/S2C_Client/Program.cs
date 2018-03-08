fb5bb929120537a1f9629b0de694a07055997977
nguyentan11 <35859403+nguyentan11@users.noreply.github.com>
2018-03-08 22:37:49 +0700

    Add files via upload

---
+++ S2C_Client/S2C_Client/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace S2C_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient();
+
+            client.Connect("127.0.0.1", 1234);
+            Stream stream = client.GetStream();
+
+            Console.WriteLine("Da ket noi toi server.");
+            while (true)
+            {
+                Console.Write("Nhap ten cua ban: ");
+
+                string str = Console.ReadLine();
+                var reader = new StreamReader(stream);
+                var writer = new StreamWriter(stream);
+                writer.AutoFlush = true;
+
+                writer.WriteLine(str);
+                str = reader.ReadLine();
+                Console.WriteLine(str);
+                if (str.ToUpper() == "BYE")
+                    break;
+            }
+            stream.Close();
+            client.Close();
+        }
+    }
+}
---
+++ S2C_Server/S2C_Server/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace S2C_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TcpListener listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 1234);
+
+            listener.Start();
+
+            Console.WriteLine("Server khoi dong o: " + listener.LocalEndpoint);
+            Console.WriteLine("Dang doi ket noi...");
+
+            Socket socket = listener.AcceptSocket();
+            Console.WriteLine("Ket noi toi " + socket.RemoteEndPoint);
+
+            var stream = new NetworkStream(socket);
+            var reader = new StreamReader(stream);
+            var writer = new StreamWriter(stream);
+            writer.AutoFlush = true;
+
+            while (true)
+            {
+                string str = reader.ReadLine();
+                if (str.ToUpper() == "EXIT")
+                {
+                    writer.WriteLine("bye");
+                    break;
+                }
+                writer.WriteLine("Hello " + str);
+            }
+            stream.Close();
+            socket.Close();
+            listener.Stop();
+        }
+    }
+}
